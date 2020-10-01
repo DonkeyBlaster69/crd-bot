915051b984d09186c61a8054786b87c5c24c1af8
DonkeyBlaster69 <ryanzhou2004@gmail.com>
2020-09-30 21:10:47 -0700

    fixed a bug in russian roulette

--- cbgames.py
+++ cbgames.py
@@ -93,7 +93,7 @@ class CBgames(commands.Cog):
         else:
             if operation == 'new':
                 funcs.removecb(context.author.id, amount)
-                c.execute("INSERT INTO russianroulette(gameid, bet, player1, started) VALUES (?, ?, ?, 0", (gameid, amount, context.author.id))
+                c.execute("INSERT INTO russianroulette(gameid, bet, player1, started) VALUES (?, ?, ?, 0)", (gameid, amount, context.author.id))
                 embed = discord.Embed(title="Russian Roulette", color=0xffa500)
                 embed.add_field(name="Game ID", value=str(gameid), inline=True)
                 embed.add_field(name="Bet Amount", value=str(amount), inline=True)
@@ -219,7 +219,20 @@ class CBgames(commands.Cog):
         elif bal < amount:
             await funcs.insufficientcb(context, self.client)
         else:
-            pass
+            # Generate a list of card values
+            cards = {"Ace of Spades": 1, "Two of Spades": 2, "Three of Spades": 3, "Four of Spades": 4, "Five of Spades": 5, "Six of Spades": 6, "Seven of Spades": 7,
+                     "Eight of Spades": 8, "Nine of Spades": 9, "Ten of Spades": 10, "Jack of Spades": 10, "Queen of Spades": 10, "King of Spades": 10,
+
+                     "Ace of Clubs": 1, "Two of Clubs": 2, "Three of Clubs": 3, "Four of Clubs": 5, "Five of Clubs": 5, "Six of Clubs": 6, "Seven of Clubs": 7,
+                     "Eight of Clubs": 8, "Nine of Clubs": 9, "Ten of Clubs": 10, "Jack of Clubs": 10, "Queen of Clubs": 10, "King of Clubs": 10,
+
+                     "Ace of Hearts": 1, "Two of Hearts": 2, "Three of Hearts": 3, "Four of Hearts": 5, "Five of Hearts": 5, "Six of Hearts": 6, "Seven of Hearts": 7,
+                     "Eight of Hearts": 8, "Nine of Hearts": 9, "Ten of Hearts": 10, "Jack of Hearts": 10, "Queen of Hearts": 10, "King of Hearts": 10,
+
+                     "Ace of Diamonds": 1, "Two of Diamonds": 2, "Three of Diamonds": 3, "Four of Diamonds": 5, "Five of Diamonds": 5, "Six of Diamonds": 6, "Seven of Diamonds": 7,
+                     "Eight of Diamonds": 8, "Nine of Diamonds": 9, "Ten of Diamonds": 10, "Jack of Diamonds": 10, "Queen of Diamonds": 10, "King of Diamonds": 10}
+
+
 
 
 def setup(client):
