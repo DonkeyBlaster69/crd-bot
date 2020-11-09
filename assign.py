da58457bfbc8e7c87f4534608f1e3df68a9a4d07
DonkeyBlaster69 <ryanzhou2004@gmail.com>
2020-11-08 23:19:54 -0800

    Starting part for new bit of assign

--- assign.py
+++ assign.py
@@ -9,16 +9,24 @@ class Assign(commands.Cog):
         self.client = client
 
     @commands.command(name="assign")
-    @commands.check_any(commands.has_guild_permissions(administrator=True), commands.has_role(674100065043873832))
+    @commands.has_guild_permissions(administrator=True)
     async def assign(self, context):
         await context.message.add_reaction(u'\U0001f504')
+        # Get roles
         staff = context.guild.get_role(564649798196658186)
         excluded = context.guild.get_role(675395764599652362)
         tempexcluded = context.guild.get_role(675452262939885568)
         suspended = context.guild.get_role(676281863605583906)
-        staffannouncements = self.client.get_channel(656636514691973139)
+        # --
+        bot = context.guild.get_role(656885049370607644)  # Bot Developer
+        assist = context.guild.get_role(750350186877812878)  # Assistant Deputy Director
+        deputy = context.guild.get_role(658491821151748155)  # Deputy Director
+        director = context.guild.get_role(528682495089180682)  # Community Relations Director
+        overseer = context.guild.get_role(528682488613306379)  # Community Relations Overseer
+
         stafflist = []
-        qotdlist = []
+        crdqotdlist = []
+        trqotdlist = []
         for member in context.guild.members:
             if staff in member.roles:
                 if tempexcluded in member.roles:
@@ -30,17 +38,18 @@ class Assign(commands.Cog):
         # checks for dupes
         for i in range(7):
             tempchoice = random.choice(stafflist)
-            qotdlist.append(tempchoice)
+            crdqotdlist.append(tempchoice)
             stafflist.remove(tempchoice)
             await tempchoice.add_roles(tempexcluded)
         embed = discord.Embed(title="QOTD assignments", color=0x8100c2)
-        embed.add_field(name="Monday", value=qotdlist[0].mention, inline=False)
-        embed.add_field(name="Tuesday", value=qotdlist[1].mention, inline=False)
-        embed.add_field(name="Wednesday", value=qotdlist[2].mention, inline=False)
-        embed.add_field(name="Thursday", value=qotdlist[3].mention, inline=False)
-        embed.add_field(name="Friday", value=qotdlist[4].mention, inline=False)
-        embed.add_field(name="Saturday", value=qotdlist[5].mention, inline=False)
-        embed.add_field(name="Sunday", value=qotdlist[6].mention, inline=False)
+        embed.add_field(name="Monday", value=f"CRD: {crdqotdlist[0].mention}", inline=False)
+        embed.add_field(name="Tuesday", value=f"CRD: {crdqotdlist[1].mention}", inline=False)
+        embed.add_field(name="Wednesday", value=f"CRD: {crdqotdlist[2].mention}", inline=False)
+        embed.add_field(name="Thursday", value=f"CRD: {crdqotdlist[3].mention}", inline=False)
+        embed.add_field(name="Friday", value=f"CRD: {crdqotdlist[4].mention}", inline=False)
+        embed.add_field(name="Saturday", value=f"CRD: {crdqotdlist[5].mention}", inline=False)
+        embed.add_field(name="Sunday", value=f"CRD: {crdqotdlist[6].mention}", inline=False)
+        staffannouncements = self.client.get_channel(656636514691973139)
         await staffannouncements.send(embed=embed)
         await context.message.add_reaction(self.client.check)
 
