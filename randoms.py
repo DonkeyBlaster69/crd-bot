d45ad9f35fc003122c71239466e4fc6ac89df40c
DonkeyBlaster69 <ryanzhou2004@gmail.com>
2020-11-15 19:21:58 -0800

    Removed extra comma

--- randoms.py
+++ randoms.py
@@ -50,7 +50,7 @@ class Randoms(commands.Cog):
         possible_responses = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.',
                               'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.',
                               'Signs point to yes.', "Don't count on it.", 'My reply is no.', 'My sources say no.',
-                              'Outlook not so good.', 'Very doubtful.', ]
+                              'Outlook not so good.', 'Very doubtful.']
         await context.send(random.choice(possible_responses))
 
 
